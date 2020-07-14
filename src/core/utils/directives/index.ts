9dc60f8b1ec4d6a81fe72640abbcb84199f06c76
ljcastroLemons <ljcastroLemons911019@gmail.com>
2020-07-14 05:19:20 +0200

    Cleaning up

--- src/core/utils/directives/index.ts
+++ src/core/utils/directives/index.ts
@@ -1,7 +1,6 @@
 import {autocompleteOff} from "@/core/utils/directives/autocomplete-off";
 import {permisos} from "@/core/utils/directives/permisos";
 
-
 export const loadCustomDirectives = (Vue: any) => {
   Vue.directive('autocomplete-off', autocompleteOff);
   Vue.directive('permisos', permisos);
